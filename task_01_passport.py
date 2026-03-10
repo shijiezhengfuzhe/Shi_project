8aa2b9f71cc27b872249e0d154927504d416de58
shijiezhengfuzhe <86466027+shijiezhengfuzhe@users.noreply.github.com>
2026-03-10 13:21:58 +0300

    Create task_01_passport.py

---
+++ task_01_passport.py
@@ -0,0 +1,24 @@
+student_name = "Ши Юэ"
+group_number = "3140801/52501"
+project_name = "SHI"
+floors = 5
+height = 17
+is_residential = True
+construction_year = 2021
+
+if is_residential ==True:
+    type_of_build = "Жилое"
+else:
+    type_of_build = "Aдминистративное"
+
+
+construction_year = 2003
+print("=== Digital Passport of the Object ===")
+print(f"student: {student_name}")
+print(f"Group: {group_number}")
+print(f"Project: {project_name}")
+print(f"Floors ={floors}")
+print(f"Height:{height}")
+print(f"Type: {type_of_build}")
+print(f"Year of construction:{construction_year}")
+print("==============================")
