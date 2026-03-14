892287647de056d20dd20416170b492c1fd3ec3b
shijiezhengfuzhe <86466027+shijiezhengfuzhe@users.noreply.github.com>
2026-03-15 01:05:30 +0300

    Update task_01_passport.py

--- task_01_passport.py
+++ task_01_passport.py
@@ -1,8 +1,8 @@
 student_name = "Ши Юэ"
 group_number = "3140801/52501"
-project_name = "SHI"
+project_name = "新世界"
 floors = 5
-height = 17
+height = 17.2
 is_residential = True
 construction_year = 2021
 
@@ -12,11 +12,13 @@ else:
     type_of_build = "Aдминистративное"
 
 print("=== Digital Passport of the Object ===")
-print(f"student: {student_name}")
-print(f"Group: {group_number}")
-print(f"Project: {project_name}")
-print(f"Floors ={floors}")
-print(f"Height:{height}")
-print(f"Type: {type_of_build}")
-print(f"Year of construction:{construction_year}")
+print(f"Составитель: {student_name}")
+print(f"Групп: {group_number}")
+print(f"ОбЪект: {project_name}")
+print(f"Этажность ={floors} этажей")
+print(f"Высота:{height} м")
+print(f"Типа: {type_of_build}")
+print(f"Год постройки:{construction_year}")
 print("==============================")
+# Местоположение: № 1, район Чаоян, Пекин
+# Обоснование выбора: Знаковое здание
