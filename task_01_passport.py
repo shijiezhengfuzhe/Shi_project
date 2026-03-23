29c31c1b4bbf159ceb9a02cd8b35b5eb0ef9cd40
shijiezhengfuzhe <86466027+shijiezhengfuzhe@users.noreply.github.com>
2026-03-24 02:17:38 +0300

    Add files via upload

---
+++ task_01_passport.py
@@ -0,0 +1,26 @@
+student_name = "Ши Юэ"
+group_number = "3140801/52501"
+project_name = "回家"
+floors = 8
+height = 25.3
+is_residential = True
+construction_year = 2003
+
+#Формирование словаря с данными объекта
+building_passport = {
+    "Составитель": student_name,
+    "Группа": group_number,
+    "Объект": project_name,
+    "Этажность": f"{floors} этажей",
+    "Высота": f"{height} м",
+    "Тип": "Жилой" if is_residential else "Нежилой",
+    "Год постройки": construction_year
+}
+
+# Вывод в читаемом виде с использованием заголовков
+print("=== ПАСПОРТ СТРОИТЕЛЬНОГО ОБЪЕКТА ===")
+for key, value in building_passport.items():
+    print(f"{key}: {value}")
+print("=====================================")
+# Местоположение: № 1, район Чаоян, Пекин
+# Обоснование выбора: Знаковое здание
